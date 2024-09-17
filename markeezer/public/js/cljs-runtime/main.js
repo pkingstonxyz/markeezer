c130d2d3b3cc2ca1c388a007a7db954385dce275
Patrick Kingston <pkingston@bowdoin.edu>
2024-09-16 20:39:17 -0400

    fixed gitignore issue

--- markeezer/public/js/cljs-runtime/main.js
+++
@@ -1,6 +0,0 @@
-goog.provide('main');
-main.init = (function main$init(){
-return alert("hi");
-});
-
-//# sourceMappingURL=main.js.map
--- markeezer/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { main.init(); } catch (e) { console.error("An error occurred when calling (main/init)"); console.error(e); }
